aaa2c71ce546c815387c5f7e5fec9a7823571aeb
Natalie Clarius <natalie_clarius@yahoo.de>
2022-05-02 16:28:59 +0200

    add opacity

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo mkdir -p /usr/lib/plasma-hud
+sudo cp usr/lib/plasma-hud/plasma-hud /usr/lib/plasma-hud/
+sudo mkdir -p /etc/xdg/autostart
+sudo cp etc/xdg/autostart/plasma-hud.desktop /etc/xdg/autostart/
---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/usr/lib/plasma-hud/plasma-hud 
